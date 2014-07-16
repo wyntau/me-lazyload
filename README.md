53adac0a5d113cb3936278272bbe7d895cbb6baf
Jeremial <jeremial90@gmail.com>
2014-07-16 14:03:14 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## me-lazyload
+Images lazyload directive for angular.
+
+### Usage
+1. include me-lazyload.js as dependence.
+2. instead `src` with `src`.
+
+    <img lazyload="{{imgUrl}}" alt="">
+
+3. All Done!
+
+### License
+MIT
---
+++ me-lazyload.js
@@ -0,0 +1,127 @@
+angular.module('me-lazyload', [])
+.directive('lazyload', ['$window', '$document', function($window, $document){
+    var doc = $document[0],
+        body = doc.body,
+        win = $window,
+        $win = angular.element(win),
+        uid = 0,
+        elements = {};
+
+
+    function getUid(el){
+        return el.__uid || (el.__uid = '' + ++uid);
+    }
+
+    function getWindowOffset(){
+        var t,
+            pageXOffset = (typeof win.pageXOffset == 'number') ? win.pageXOffset : (((t = doc.documentElement) || (t = body.parentNode)) && typeof t.ScrollLeft == 'number' ? t : body).ScrollLeft,
+            pageYOffset = (typeof win.pageYOffset == 'number') ? win.pageYOffset : (((t = doc.documentElement) || (t = body.parentNode)) && typeof t.ScrollTop == 'number' ? t : body).ScrollTop;
+        return {
+            offsetX: pageXOffset,
+            offsetY: pageYOffset
+        };
+    }
+
+    function isVisible(iElement){
+        var elem = iElement[0],
+            elemRect = elem.getBoundingClientRect(),
+            windowOffset = getWindowOffset(),
+            winOffsetX = windowOffset.offsetX,
+            winOffsetY = windowOffset.offsetY,
+            elemWidth = elemRect.width,
+            elemHeight = elemRect.height,
+            elemOffsetX = elemRect.left + winOffsetX,
+            elemOffsetY = elemRect.top + winOffsetY,
+            viewWidth = Math.max(doc.documentElement.clientWidth, win.innerWidth || 0),
+            viewHeight = Math.max(doc.documentElement.clientHeight, win.innerHeight || 0),
+            xVisible,
+            yVisible;
+
+        if(elemOffsetY <= winOffsetY){
+            if(elemOffsetY + elemHeight >= winOffsetY){
+                yVisible = true;
+            }
+        }else if(elemOffsetY >= winOffsetY){
+            if(elemOffsetY <= winOffsetY + viewHeight){
+                yVisible = true;
+            }
+        }
+
+        if(elemOffsetX <= winOffsetX){
+            if(elemOffsetX + elemWidth >= winOffsetX){
+                xVisible = true;
+            }
+        }else if(elemOffsetX >= winOffsetX){
+            if(elemOffsetX <= winOffsetX + viewWidth){
+                xVisible = true;
+            }
+        }
+
+        return xVisible && yVisible;
+    };
+
+    function checkImage(){
+        Object.keys(elements).forEach(function(key){
+            var obj = elements[key],
+                iElement = obj.iElement,
+                $scope = obj.$scope;
+            if(isVisible(iElement)){
+                iElement.attr('src', $scope.lazySrc);
+            }
+        });
+    }
+
+    $win.bind('scroll', checkImage);
+    $win.bind('resize', checkImage);
+
+    function onLoad(){
+        var $el = angular.element(this),
+            uid = getUid($el);
+
+        $el.unbind('load');
+        $el.css('opacity', 1);
+        $el.__loaded = true;
+
+        if(elements.hasOwnProperty(uid)){
+            delete elements[uid];
+        }
+
+        if(Object.keys(elements).length === 0){
+            $win.unbind('scroll');
+            $win.unbind('resize');
+        }
+    }
+
+    return {
+        restrict: 'A',
+        scope: {
+            lazySrc: '@lazyload'
+        },
+        link: function($scope, iElement, iAttributes){
+
+            iElement.bind('load', onLoad);
+
+            $scope.$watch('lazySrc', function(){
+                if(iElement.__isloaded || isVisible(iElement)){
+                    iElement.attr('src', $scope.lazySrc);
+                }else{
+                    var uid = getUid(iElement);
+                    iElement.css({
+                        'background-color': '#fff',
+                        'opacity': 0,
+                        '-webkit-transition': 'opacity 1s',
+                        'transition': 'opacity 1s'
+                    });
+                    elements[uid] = {
+                        iElement: iElement,
+                        $scope: $scope
+                    };
+                }
+            });
+
+            $scope.$on('$destroy', function(){
+                iElement.unbind('load');
+            });
+        }
+    };
+}]);
